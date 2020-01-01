487bf8bcb8e3c5020b01456c772cfa92012fd443
KazuyaMiayshita <miy1924@gmail.com>
2020-01-01 09:18:06 +0900

    dependencies, scalafmt, docker

--- build.sbt
+++ build.sbt
@@ -5,10 +5,27 @@ ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "com.example"
 ThisBuild / organizationName := "example"
 
+lazy val commonSettings = List(
+  scalacOptions ++= "-deprecation" :: "-feature" :: "-Xlint" :: Nil,
+  scalacOptions in (Compile, console) ~= {_.filterNot(_ == "-Xlint")},
+  scalafmtOnCompile := true
+)
+
 lazy val root = (project in file("."))
   .settings(
     name := "akkahttp-twitter",
-    libraryDependencies += scalaTest % Test
+    commonSettings,
+    libraryDependencies ++= List(
+      scalaTest.map(_%Test),
+      scalaMock.map(_%Test),
+      akkaHttp,
+      cats,
+      circe,
+      guice,
+      typesafeConfig,
+      quill,
+      mysql
+    ).flatten
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,18 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8" :: Nil
+  lazy val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0" :: Nil
+  lazy val akkaHttp = "com.typesafe.akka" %% "akka-http" % "10.1.11" :: Nil
+  lazy val cats = 
+    "org.typelevel" %% "cats-core" % "2.0.0" ::
+    "org.typelevel" %% "cats-effect" % "2.0.0" :: Nil
+  lazy val circe = 
+    "io.circe" %% "circe-core" % "0.12.3" ::
+    "io.circe" %% "circe-generic" % "0.12.3" ::
+    "io.circe" %% "circe-parser" % "0.12.3" :: Nil
+  lazy val guice = "com.google.inject" % "guice" % "4.0" :: Nil
+  lazy val typesafeConfig = "com.typesafe" % "config" % "1.4.0" :: Nil
+  lazy val quill = "io.getquill" %% "quill-core" % "3.5.0" :: Nil
+  lazy val mysql = "mysql" % "mysql-connector-java" % "8.0.18" :: Nil
 }
